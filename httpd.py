0feb90a02b13ad00f865f564e4d249a5c684d8ec
s191k <solnushkoon@mail.ru>
2019-08-09 12:35:38 +0300

    rework httpd.py clean issues

--- httpd.py
+++ httpd.py
@@ -1,10 +1,15 @@
+import concurrent
 import datetime
 import logging
 import os
+import queue
 import re
 import socket
 import argparse
-
+import threading
+from multiprocessing.dummy import Pool as ThreadPool
+import concurrent
+import concurrent.futures
 
 class OtusServer:
     SOCKET_SERVER_HOST = 'localhost'
@@ -14,6 +19,7 @@ class OtusServer:
     MAX_LISTENER_NUMBER = 5
     DOCUMENT_ROOT = 'DOCUMENT_ROOT'
     empty_values = (None,(),[],{})
+    WORKERS_AMOUNT = 1
 
     #200 403 404 405
     OK_HEADER = b'HTTP/1.x 200 OK\r\n'
@@ -35,7 +41,8 @@ class OtusServer:
         result_args = argument_parser.parse_args()
         if result_args.r not in OtusServer.empty_values:
             OtusServer.DOCUMENT_ROOT = result_args.r
-        # workers = result_args.w
+        if result_args.w not in OtusServer.empty_values:
+            OtusServer.WORKERS_AMOUNT = result_args.w
 
     def get_content_type(self, path):
         if path.endswith('/') or path.endswith('.html') or path.count('.') == 0:
@@ -60,15 +67,7 @@ class OtusServer:
             user_socket.send(OtusServer.OK_HEADER)
             user_socket.send(b'Date: ' + str(datetime.datetime.now()).encode('utf-8') + b'\r\n') #date_header
             user_socket.send(OtusServer.SERVER_HEADER)
-            ## TODO
-            try:
-                print(path)
-                user_socket.send(b'ContentLength: ' + str(os.path.getsize(path)).encode() + b'\r\n')
-            except:
-                print('dont put contentlength')
-
-
-            # user_socket.send(b'ContentLength: ' + str(os.path.getsize(path)).encode() + b'\r\n')
+            user_socket.send(b'ContentLength: ' + str(os.path.getsize(path)).encode() + b'\r\n')
             user_socket.send(b'Connection: close\r\n') ## close? ## Is that right?? What need to write ??
         if code == 403: user_socket.send(OtusServer.FORBIDDEN_REQUEST_HEADER)
         if code == 404: user_socket.send(OtusServer.NOT_FOUND_HEADER)
@@ -77,15 +76,11 @@ class OtusServer:
         user_socket.send(self.get_content_type(path))
         user_socket.send(b'\r\n')
 
-    def cust_html_to_byte(self, html_str):
+    def casting_html_to_byte(self, html_str):
         return html_str.replace(b'\n',b'').replace(b'\r',b'').replace(b'\t',b'')
 
     def is_path_exist(self, path_to_page):
-        try:
-            return os.path.exists(OtusServer.DOCUMENT_ROOT + path_to_page)
-        except Exception as ex:
-            logging.error('Get exception while finding your page')
-            logging.error(ex)
+        return os.path.exists(OtusServer.DOCUMENT_ROOT + path_to_page)
 
     def _get_result_path_page_helper(self,path_to_page):
         path_array = path_to_page.replace('/', ' ').split()
@@ -102,12 +97,12 @@ class OtusServer:
     def get_page(self, user_socket, path_to_page):
         if self.is_path_exist(path_to_page):
             result_path = self._get_result_path_page_helper(path_to_page)
-            self.send_html_header(user_socket,result_path,200) #Отправляем заголовок
+            self.send_html_header(user_socket,result_path,200)
             with open(result_path, 'rb') as html_page:
                 file_str = html_page.read()
-                user_socket.sendall(self.cust_html_to_byte(file_str))
+                user_socket.sendall(self.casting_html_to_byte(file_str))
         else:
-            self.send_html_header(user_socket,path_to_page, 404) #Отправляем заголовок
+            self.send_html_header(user_socket,path_to_page, 404)
 
     def get_file(self, path, user_socket):
         with open(path, 'rb') as file:
@@ -117,7 +112,7 @@ class OtusServer:
         str_data = request.decode('utf-8')
         method, path, type = str_data.split('\n')[0].split()
         if method == "GET" or method == "HEAD":
-            if re.search(r'(\.css|\.js|\.jpg|\.jpeg|\.png|\.gif|\.swf)', path): #Нет проверок
+            if re.search(r'(\.css|\.js|\.jpg|\.jpeg|\.png|\.gif|\.swf)', path) and method == "GET":
                 temp = str_data.split('\n')
                 #searching referer_url -- need get package name
                 for temp_line in temp:
@@ -125,7 +120,7 @@ class OtusServer:
                         referer = temp_line.split()[1].replace('\r','').replace('\n','').replace('\t','')
                 full_url_path = (referer + path).replace((r'http://' + OtusServer.SOCKET_SERVER_HOST + ':' + str(OtusServer.SOCKET_SERVER_PORT)),'')
                 path = OtusServer.DOCUMENT_ROOT + full_url_path.replace('/','\\')
-                if path.count('%20') > 0: path = path.replace('%20', '')  # Единичный случай
+                if path.count('%20') > 0: path = path.replace('%20', '')
                 self.send_html_header(user_socket, path, 200)
                 self.get_file(path=path, user_socket=user_socket)
             else:
@@ -134,26 +129,45 @@ class OtusServer:
             self.send_html_header(user_socket,path, 405)
             user_socket.sendall(b"that's ERROR-request")
 
+    def _get_client_response(self,connection):
+        data = b''
+        while True:
+            chunk = connection.recv(1024)
+            data += chunk
+            if not chunk or chunk.endswith(b'\r\n\r\n'):
+                break
+        return data
+
     def run_server(self):
         serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         serv_socket.bind(OtusServer.SOCKET_SERVER)
         serv_socket.listen(OtusServer.MAX_LISTENER_NUMBER)
-        self.arg_parser() #Смотрим аргументы
+        self.arg_parser()
         logging.info(f'Server start work: {OtusServer.SOCKET_SERVER_HOST}:{OtusServer.SOCKET_SERVER_PORT}')
+
+        #TODO
+        # Если соединение не закрывается - то страница как бы и не загрузилась.
         try:
-            while True:
-                connection, address = serv_socket.accept()
-                try:
-                    # connection, address = serv_socket.accept()
-                    logging.info(f"new connection from {address}")
-                    data = connection.recv(1024) ##Захватить весь запрос
-                    if data:
-                        self.method_handler(request=data, user_socket=connection)
-                    logging.info(f'close {address}')
-                except Exception as ex:
-                    logging.error(ex)
-                finally:
-                    connection.close()
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                while True:
+                    try:
+                        connection, address = serv_socket.accept()
+
+                        # connection.settimeout(5)
+                        ## Это ужас надо чтобы соединение закрывалось
+                        # почему оно не закрывается??
+
+                        logging.info(f"new connection from {address}")
+                        data = self._get_client_response(connection)
+
+                        if data:
+                            executor.submit(self.method_handler, data, connection)
+                        logging.info(f'close {address}')
+                    except Exception as ex:
+                        logging.error(ex)
+                    finally:
+                        connection.close()
         except Exception as ex:
             logging.error(ex)
         finally:
